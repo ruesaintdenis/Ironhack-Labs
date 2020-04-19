e5db06a98acccc97344bd4a1623f0dd53df71d24
Denisse Disla <denisssse@gmail.com>
2020-04-19 16:54:01 -0400

    finalized sql

--- module-1/Advanced-mySQL/mod_practice.sql
+++ module-1/Advanced-mySQL/mod_practice.sql
@@ -24,3 +24,25 @@ join t_salaries s on e.emp_no = s.emp_no
 where year(de.from_date) <2002
 group by e.gender, d.dept_name, year(de.from_date)
 order by year(de.from_date);
+
+-- Create an SQL stored procedure that will allow you to obtain the average male and female salary per department within a certain salary range. 
+-- Let this range be defined by two values the user can insert when calling the procedure.
+
+drop procedure if exists gen_salary; 
+
+delimiter $$ 
+
+create procedure gen_salary(IN p_min_sal float, IN p_max_sal float)
+begin
+select e.gender, avg(s.salary), d.dept_name
+from t_employees e
+join t_salaries s on e.emp_no = s.emp_no
+join t_dept_emp de on de.emp_no = e.emp_no
+join t_departments d on d.dept_no = de.dept_no
+where s.salary between p_min_sal and p_max_sal
+group by d.dept_no, e.gender; 
+end $$
+
+delimiter ; 
+
+call gen_salary(50000,90000);
